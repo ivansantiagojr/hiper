5be2d8bc642af59e63f206f5a8628a6c5e05fe9a
ivansantiagojr <ivansantiago.junior@gmail.com>
2026-06-10 01:26:13 -0300

    Decide on router registry
    the format is a list of tables having: method, path, and handler

--- spec/hiper_spec.lua
+++ spec/hiper_spec.lua
@@ -1,25 +1,40 @@
-describe("Test base hiper methods", function ()
-    describe("serve method is available", function ()
-        it("should contain a serve method", function ()
-            local Hiper = require("hiper")
+local Hiper = require('hiper')
+
+describe('Test base hiper methods', function()
+    describe('serve method is available', function()
+        it('should contain a serve method', function()
             local app = Hiper:new()
 
             assert.is_not_nil(app.serve)
         end)
 
-        it("should contain a server property", function ()
-            local Hiper = require("hiper")
+        it('should contain a server property', function()
             local app = Hiper:new()
 
             assert.is_not_nil(app.server)
         end)
 
-        it("should have different server property", function ()
-            local Hiper = require("hiper")
+        it('should have different server property', function()
             local app = Hiper:new()
             local other_app = Hiper:new()
 
             assert.are_not_equals(app.server, other_app.server)
+        end)
+    end)
+    describe('Test hiper get router', function()
+        it('should register to routers', function()
+            local app = Hiper:new()
+
+            local path = '/path'
+            local handler = function() end
+            app:get(path, handler)
+
+            assert.is_true(#app.routes == 1)
+
+            local expected_route = { method = 'GET', handler = handler, path = path }
+            local actual_route = app.routes[1]
+
+            assert.are.same(actual_route, expected_route)
         end)
     end)
 end)
--- src/hiper/init.lua
+++ src/hiper/init.lua
@@ -1,25 +1,30 @@
 local Hiper = {}
 Hiper.__index = Hiper
 
-function Hiper:new(params)
-    params = params or {}
+function Hiper:new()
+    local app = {}
+
+    app.routes = {}
 
     local pegasus = require('pegasus')
 
-    local instance = setmetatable({}, Hiper)
-
-    instance.server = pegasus:new({
+    app.server = pegasus:new({
         port = '9090',
         location = 'example/root',
     })
 
-    return instance
+    return setmetatable(app, Hiper)
 end
 
 function Hiper:serve()
     self.server:start(function(request, response)
         print("It's running...")
     end)
+end
+
+function Hiper:get(path, handler)
+    local route = { method = 'GET', path = path, handler = handler }
+    table.insert(self.routes, route)
 end
 
 return Hiper
